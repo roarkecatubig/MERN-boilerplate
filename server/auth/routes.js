ff0a4dc541bf0e4a9de8cb3e2045eed66f86ef76
AaronMcCloskey <mcclosa@gmail.com>
2017-09-03 22:46:43 +0100

    Add google OAuth with callback

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -4,9 +4,17 @@ var router = require('express').Router();
 var verifyUser = require('./auth').verifyUser;
 // Require auth controller
 var controller = require('./controller');
+// Require Passport
+var passport = require('passport');
 
 // before we send back a jwt, lets check
 // the password and username match what is in the DB
 router.post('/signin', verifyUser(), controller.signin);
+// Google OAuth, send request to google
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+// Retreive code from google OAuth
+router.get('/google/callback', passport.authenticate('google'));
 
 module.exports = router;
