ff737aa29588255bc8c77fbcaf04eebce09a7c84
AaronMcCloskey <mcclosa@gmail.com>
2017-09-03 23:51:08 +0100

    Temporarily prevent seeding db

--- server/config/development.js
+++ server/config/development.js
@@ -2,7 +2,7 @@ module.exports = {
     // enabled logging for development
     logging: true,
     // seed the db
-    seed: true,
+    seed: false,
     // set db to localhost/dbname
     db: {
         url: 'mongodb://localhost/merndb'
