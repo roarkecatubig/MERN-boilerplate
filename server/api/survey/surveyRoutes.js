5017f4abd8149c85f44e7be51703b9b9f933cbc3
AaronMcCloskey <mcclosa@gmail.com>
2017-09-05 00:31:13 +0100

    Setup Survey Routes file

---
+++ server/api/survey/surveyRoutes.js
@@ -0,0 +1,14 @@
+//Set variable name for Router to be called in createRoutes utility
+var router = require('express').Router();
+// Require Controller
+var controller = require('./surveyController');
+// Require router utility
+var createRoutes = require('../../util/createRoutes');
+// Require logger for logging to console
+var logger = require('../../util/logger');
+
+// Run router utility
+// pass controller & router variables
+createRoutes(controller, router);
+
+module.exports = router;
