215e59f702b09537352c8826ab729617aa3bb87a
AaronMcCloskey <mcclosa@gmail.com>
2017-09-04 19:06:32 +0100

    Check if production for return keys

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+// Figure out what credentials to return
+if (process.env.NODE_ENV === 'production') {
+    // We are in production, return prod set of keys
+    module.exports = require('./prod');
+} else {
+    // We are in development , return dev set of keys
+    module.exports = require('./dev');
+}
