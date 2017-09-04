eaa3a48574c7b94234bc08266ca89998a293bbfe
AaronMcCloskey <mcclosa@gmail.com>
2017-09-04 19:07:08 +0100

    Set up environment variables for productions

---
+++ server/config/prod.js
@@ -0,0 +1,7 @@
+// Production Keys here
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    jwtSecret: process.env.JWT_SECRET,
+    cookieKey: process.env.COOKIE_KEY
+};
