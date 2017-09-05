a0d0788fe8443dad3fb92225a14c9896595a2e63
AaronMcCloskey <mcclosa@gmail.com>
2017-09-05 19:21:28 +0100

    Add require login middleware

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if (!req.user) {
-        return res.status(401).send({ error: 'You must log in to access' });
+        return res.status(401).send('You must be logged in to access');
     }
     next();
 }
