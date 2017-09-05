66dbfa35b187b8a4cb01c885d6285c65f52da175
AaronMcCloskey <mcclosa@gmail.com>
2017-09-05 17:22:46 +0100

    Add routes to surveyRoutes

--- server/api/survey/surveyRoutes.js
+++ server/api/survey/surveyRoutes.js
@@ -2,13 +2,27 @@
 var router = require('express').Router();
 // Require Controller
 var controller = require('./surveyController');
-// Require router utility
-var createRoutes = require('../../util/createRoutes');
 // Require logger for logging to console
 var logger = require('../../util/logger');
 
-// Run router utility
-// pass controller & router variables
-createRoutes(controller, router);
+router.param('id', controller.params);
+
+// Define routes
+// All requests to '/api/router'
+// Go to relative controller and selected method
+router.route('/')
+    // .get(controller.get)
+    // no auth, users need to be able to sign up/login
+    .post(controller.post);
+
+// router.route('/:id')
+//     .get(controller.getOne)
+//     // add auth, check if updated user is current user
+//     .put(checkUser, validUser, controller.put)
+//     // add auth, check if user to be deleted is current user
+//     .delete(checkUser, validUser, controller.delete)
+
+// // Add router for api/users/me
+// router.get('/me', checkUser, controller.me);
 
 module.exports = router;
