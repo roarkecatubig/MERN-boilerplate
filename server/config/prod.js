5266b9e993b55d769149abe9da3bc1e24825af1d
AaronMcCloskey <mcclosa@gmail.com>
2017-09-05 19:21:58 +0100

    Add send grid key

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     jwtSecret: process.env.JWT_SECRET,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    sendGridKey: process.env.SEND_GRID_KEY
 };
