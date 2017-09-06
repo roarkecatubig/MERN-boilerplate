91f29bbbce78b2121308a0959e8541a26111dcaf
AaronMcCloskey <mcclosa@gmail.com>
2017-09-06 01:58:07 +0100

    Add current_user auth route

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -20,5 +20,9 @@ router.get('/logout', (req, res) => {
     req.logout();
     res.send(req.user);
 });
+router.get('/current_user', (req, res) => {
+    res.send(req.user);
+});
+
 
 module.exports = router;
