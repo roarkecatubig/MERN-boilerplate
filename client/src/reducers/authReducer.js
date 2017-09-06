293d78a80dc568b96b9f6f248181b9150b99d4d9
AaronMcCloskey <mcclosa@gmail.com>
2017-09-06 02:57:57 +0100

    Add switch case to authReducer to check if type of FETCH_USER is true

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,14 +1,20 @@
-// import { FETCH_USER } from '../actions/types';
+// import action type
+import { FETCH_USER } from '../actions/types';
 
-// 1st Argument, state object responsible for this reducer
+// 1st Argument, state object responsible for this reducer, 
+// default to null to avoid UI elements changing during action if request takes longer than page load
 // 2nd Argument, action object 
 export default function (state = null, action) {
     // Switch over the actions type
     switch (action.type) {
-        //     case FETCH_USER:
-        //         return action.payload || false;
+        // if there is a type of FETCH_USER
+        // user is logged in, return user model
+        // or false if payload is empty
+        case FETCH_USER:
+            return action.payload || false;
         default:
             // If no action, no change in state is necessary
+            // Intially null as set above
             return state;
     }
 }
