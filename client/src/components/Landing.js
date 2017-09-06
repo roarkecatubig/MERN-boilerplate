8232a15735a9c04ef966fa69cd8b497144efc2e6
AaronMcCloskey <mcclosa@gmail.com>
2017-09-06 03:41:23 +0100

    Create Landing page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                MERN Boilerplate
+            </h1>
+            Collect feedback from your users
+        </div>
+    );
+}
+
+export default Landing;
